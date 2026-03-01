784a199c9dddc90df7835437a1e25249323829ef
Sakina <sakinakheraj@gmail.com>
2026-03-01 23:48:04 +0530

    Fixed SpeedInsights import for Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -8,5 +8,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <SpeedInsights />
-  </StrictMode>,
+  </StrictMode>
 )
